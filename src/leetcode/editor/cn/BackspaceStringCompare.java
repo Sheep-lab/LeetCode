8556e3f5c37aecfb0db167f75a1a1ad27f221263
Sheep <21011210002@stu.xidian.edu.cn>
2022-10-21 18:55:29 +0800

    双指针 first commit!

--- src/leetcode/editor/cn/BackspaceStringCompare.java
+++ src/leetcode/editor/cn/BackspaceStringCompare.java
@@ -29,6 +29,43 @@ public class BackspaceStringCompare{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean backspaceCompare(String s, String t) {
+        int i=s.length()-1;
+        int j=t.length()-1;
+        int s_Skip=0,t_Skip=0;
+        while (i>=0||j>=0){
+            while (i>=0){
+                if (s.charAt(i)=='#'){
+                    s_Skip++;
+                    i--;
+                }else if (s_Skip>0){
+                    s_Skip--;
+                    i--;
+                }else
+                    break;
+            }
+            while (j>=0){
+                if (t.charAt(j)=='#'){
+                    t_Skip++;
+                    j--;
+                }else if (t_Skip>0){
+                    t_Skip--;
+                    j--;
+                }else
+                    break;
+            }
+            if (i>=0&&j>=0){
+                if (s.charAt(i)!=t.charAt(j))
+                    return false;
+            }else {
+                if (i>=0||j>=0)
+                    return false;
+            }
+
+            i--;
+            j--;
+        }
+
+        return true;
 
     }
 }
